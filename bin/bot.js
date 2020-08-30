e39b8c5dcad9c4b9661f3ac7bcb6d78852aedbf4
Jonathan Dietrich <dietrichj@cip.ifi.lmu.de>
2020-08-31 00:47:29 +0200

    chore: map with swtich construct instead of if/else

--- bin/bot.js
+++ bin/bot.js
@@ -133,87 +133,93 @@ function resolveSpecialCommands(channel, user, message) {
   if (resolveAdminCommands(channel, user, message)) {
     return true;
   }
-  let comms = config.commands;
-  if (comms.personalScore.toLowerCase() === message) {
-    log.info("User \"" + user + "\" sent command to get own score");
-    store.readForUser(user, function (data) {
-      client.say(channel, parseLocaleString(lang.commandScore, {
-        user: user,
-        scoreNumber: data
-      }));
-    });
-    return true;
-  } else if (comms.currentQuestion.toLowerCase() === message) {
-    log.info("User \"" + user + "\" sent message to get current question");
-    if (!questions.isQuestionAvailable()) {
-      if (config.reactToNoQuestion) {
-        client.say(target, parseLocaleString(lang.noQuestion, {
-          user: chatSender
+  const comms = new Map(Object.entries(config.commands).map(([k,v]) => ([v.toLowerCase(),k])));
+  switch(comms.get(message)) {
+    case "personalScore":
+      log.info("User \"" + user + "\" sent command to get own score");
+      store.readForUser(user, function (data) {
+        client.say(channel, parseLocaleString(lang.commandScore, {
+          user: user,
+          scoreNumber: data
         }));
+      });
+      return true;
+    case "currentQuestion":
+      log.info("User \"" + user + "\" sent message to get current question");
+      if (!questions.isQuestionAvailable()) {
+        if (config.reactToNoQuestion) {
+          client.say(target, parseLocaleString(lang.noQuestion, {
+            user: chatSender
+          }));
+        } else {
+          log.debug(
+              "Not reacting to no question as it is disabled in the config");
+        }
       } else {
-        log.debug(
-            "Not reacting to no question as it is disabled in the config");
+        client.say(channel, parseLocaleString(lang.askQuestion, {
+          question: questions.getCurrentQuestion().question,
+          answerPrefix: config.answerPrefix
+        }));
       }
-    } else {
-      client.say(channel, parseLocaleString(lang.askQuestion, {
-        question: questions.getCurrentQuestion().question,
-        answerPrefix: config.answerPrefix
-      }));
-    }
-    return true;
+      return true;
+    default:
+      return false;
   }
-  return false;
 }
 
 function resolveAdminCommands(channel, user, message) {
-  let comms = config.adminCommands;
-  if (comms.allScores.toLowerCase() === message) {
-    if (config.channelAdmin === user) {
-      log.info("Admin user \"" + user + "\" sent command to get all scores");
-      store.readAll(function (data) {
-        _sendMultilineScores(channel, data);
-      });
+  const comms = new Map(Object.entries(config.adminCommands).map(([k,v]) => ([v.toLowerCase(),k])));
+
+  const isAdminUser = config.channelAdmin === user;
+  const adminCommandIssued = comms.has(message);
+
+  if (adminCommandIssued) {
+    if (isAdminUser) {
+      switch(comms.get(message)) {
+        case "allScores":
+          log.info("Admin user \"" + user + "\" sent command to get all scores");
+          store.readAll(function (data) {
+            _sendMultilineScores(channel, data);
+          });
+          break;
+        case "reset":
+          log.info("Admin user \"" + user + "\" sent command to reset all scores");
+          store.resetStore(function (data) {
+            client.say(channel, parseLocaleString(lang.commandReset, {}));
+            _sendMultilineScores(channel, data);
+          });
+          break;
+        case "start":
+          log.info("Admin user \"" + user + "\" sent command to start bot");
+          running = true;
+          client.say(channel,
+              parseLocaleString("Starting Quiz bot. Question interval: "
+                  + "${inter}; Next question in ${next}", {
+                inter: timeConverter.forSeconds(
+                    config.postQuestionIntervalInSeconds),
+                next: timeConverter.forSeconds(
+                    questions.getSecondsUntilNextQuestion())
+              }));
+          log.info("Started bot");
+          break;
+        case "stop":
+          log.info("Admin user \"" + user + "\" sent command to stop bot");
+          running = false;
+          client.say(channel,
+              "Stopping Bot. Will not react to anything but commands");
+          log.info("Stopped bot");
+          return false;
+        default:
+          return false;
+      }
+      return true;
+    } else {
+      log.warn("Invalid user tried to execute admin command. User: \""
+          + user + "\"; Command: \"" + message + "\"");
       return true;
     }
-  } else if (comms.reset.toLowerCase() === message) {
-    if (config.channelAdmin === user) {
-      log.info("Admin user \"" + user + "\" sent command to reset all scores");
-      store.resetStore(function (data) {
-        client.say(channel, parseLocaleString(lang.commandReset, {}));
-        _sendMultilineScores(channel, data);
-      });
-      return true;
-    }
-  } else if (comms.start.toLowerCase() === message) {
-    if (config.channelAdmin === user) {
-      log.info("Admin user \"" + user + "\" sent command to start bot");
-      running = true;
-      client.say(channel,
-          parseLocaleString("Starting Quiz bot. Question interval: "
-              + "${inter}; Next question in ${next}", {
-            inter: timeConverter.forSeconds(
-                config.postQuestionIntervalInSeconds),
-            next: timeConverter.forSeconds(
-                questions.getSecondsUntilNextQuestion())
-          }));
-      log.info("Started bot");
-      return true;
-    }
-  } else if (comms.stop.toLowerCase() === message) {
-    if (config.channelAdmin === user) {
-      log.info("Admin user \"" + user + "\" sent command to stop bot");
-      running = false;
-      client.say(channel,
-          "Stopping Bot. Will not react to anything but commands");
-      log.info("Stopped bot");
-      return false;
-    }
-  } else {
-    return false;
   }
-  log.warn("Invalid user tried to execute admin command. User: \""
-      + user + "\"; Command: \"" + message + "\"");
-  return true;
+  return false;
 }
 
 function _sendMultilineScores(channel, data) {
