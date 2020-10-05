a2d893992075cc1eedb5b5139017e4f53a6f5b87
cwild <c.wild@ftapi.com>
2020-10-05 13:45:53 +0200

    chore: Additional logging for stream online check

--- bin/channelHelper.js
+++ bin/channelHelper.js
@@ -13,6 +13,7 @@ let cachedChannelId;
 async function _findChannelId() {
   let channelName = config.channelName;
   let url = FIND_CHANNEL_URL.replace("${channelName}", channelName);
+  log.debug("Trying to find channelId with url: " + url);
   let response;
   try {
     response = await axios.get(
@@ -23,24 +24,30 @@ async function _findChannelId() {
             'Authorization': 'OAuth ' + token
           }
         });
+    log.debug("Found response body when trying to find channelId: " + JSON.stringify(response.data));
   } catch (error) {
     log.error('Could not find channelId for channel "' + channelName + '": '
         + JSON.stringify(error.response.data));
     return;
   }
   cachedChannelId = response.data.users[0]._id;
+  log.debug('Found and cached channelId: "' + cachedChannelId + '"');
 }
 
 async function isChannelOnline() {
   if (!cachedChannelId) {
+    log.debug("Did not have channelId cached. Loading it");
     await _findChannelId();
+    log.debug('Finished loading channelID: "' + cachedChannelId + '"');
   }
   if (!cachedChannelId) {
     // If there was an error with finding the channel id we just assume its online
+    log.debug("There seems to have been an error with loading the channelId, as it is null");
     return true;
   }
 
   let url = FIND_STREAM_URL.replace('${channelId}', cachedChannelId);
+  log.debug("Trying to find stream data with url: " + url);
   let response;
   try {
     response = await axios.get(
@@ -51,12 +58,15 @@ async function isChannelOnline() {
             'Authorization': 'OAuth ' + token
           }
         });
+    log.debug("Found response body when trying to find channelId: " + JSON.stringify(response.data));
   } catch (error) {
     log.error('Could not find stream for channelId "' + cachedChannelId + '": '
         + JSON.stringify(error.response.data));
     return true;
   }
-  return response.data.stream != null;
+  let isOnline = response.data.stream != null;
+  log.debug("Stream is online: " + isOnline);
+  return isOnline;
 }
 
 module.exports.isChannelOnline = isChannelOnline;
