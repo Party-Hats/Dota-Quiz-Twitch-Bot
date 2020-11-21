747e748fbde728ee9fc256d76583e5ca3eefc88f
Jonathan Dietrich <dietrichj@cip.ifi.lmu.de>
2020-11-21 14:30:42 +0100

    feature: dockerize node app

--- bin/log.js
+++ bin/log.js
@@ -1,8 +1,11 @@
 const utils = require('bin/utils');
 const rfs = require("rotating-file-stream");
+const fs = require("fs");
 
 const infoLogPath = 'log/info.log';
 const debugLogPath = 'log/debug.log';
+
+const runningInDocker = _isDockerized();
 
 const infoLog = rfs.createStream(infoLogPath, {
   size: "10M",
@@ -19,24 +22,53 @@ function _doLog(file, message) {
   file.write(message + "\n");
 }
 
+function _isDockerized() {
+  try {
+    if (fs.existsSync('/.dockerenv')) {
+      return true
+    }
+  } catch (err) {
+    console.error(err);
+    return false
+  }
+}
+
 function debug(message) {
-  let logMessage = utils.currentTimeString() + " [DEBUG] " + message;
-  debugLog.write(logMessage + "\n");
+  if (process.env.NODE_ENV != "production") {
+    let logMessage = utils.currentTimeString() + " [DEBUG] " + message;
+    if (runningInDocker) {
+      console.log(logMessage);
+    } else {
+      debugLog.write(logMessage + "\n");
+    }
+  }
 }
 
 function info(message) {
   let logMessage = utils.currentTimeString() + " [INFO]  " + message;
-  _doLog(infoLog, logMessage);
+  if (runningInDocker) {
+    console.log(logMessage);
+  } else {
+    _doLog(infoLog, logMessage);
+  }
 }
 
 function warn(message) {
   let logMessage = utils.currentTimeString() + " [WARN]  " + message;
-  _doLog(infoLog, logMessage);
+  if (runningInDocker) {
+    console.warn(logMessage);
+  } else {
+    _doLog(infoLog, logMessage);
+  }
 }
 
 function error(message) {
   let logMessage = utils.currentTimeString() + " [ERROR] " + message;
-  _doLog(infoLog, logMessage);
+  if (runningInDocker) {
+    console.error(logMessage);
+  } else {
+    _doLog(infoLog, logMessage);
+  }
 }
 
 module.exports.debug = debug;
